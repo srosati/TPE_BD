8a0858677902ea6277a7c37604cae5a3d3b5ff91
matiaslombardi <58432354+matiaslombardi@users.noreply.github.com>
2021-06-06 12:41:10 -0300

    Median Half-Working

---
+++ funciones.sql
@@ -0,0 +1,101 @@
+--1 Tabla intermedia
+
+CREATE TABLE intermedia
+(
+    Quarter       text not null,
+    Month         text not null,
+    Week          text not null,
+    Product_type  text not null,
+    Territory     text not null,
+    Sales_Channel text not null,
+    Customer_type text not null,
+    Revenue       float,
+    Cost          float,
+    PRIMARY KEY (Quarter, Month, Week, Product_type, Territory, Sales_Channel, Customer_type)
+);
+
+--2 Tabla definitiva
+
+CREATE TABLE definitiva
+(
+    Sales_Date    date not null,
+    Product_type  text not null,
+    Territory     text not null,
+    Sales_Channel text not null,
+    Customer_type text not null,
+    Revenue       float,
+    Cost          float,
+    PRIMARY KEY (Sales_Date, Product_type, Territory, Sales_Channel, Customer_type)
+);
+
+--3 Importación de datos
+
+CREATE OR REPLACE FUNCTION insertarEnDefinitiva() RETURNS TRIGGER
+AS
+$$
+DECLARE
+    aDay   int;
+    aMonth int;
+    aYear  int;
+BEGIN
+    aDay := CASE substr(new.Week, 1, 2)
+                WHEN 'W1' THEN 1
+                WHEN 'W2' THEN 8
+                WHEN 'W3' THEN 15
+                WHEN 'W4' THEN 22
+                WHEN 'W5' THEN 29
+        END;
+    aMonth := EXTRACT(MONTH FROM TO_DATE(substr(new.Month, 4, 3), 'Mon'));
+    aYear := substr(new.Quarter, 4, 4)::INTEGER;
+
+    INSERT INTO definitiva
+    VALUES (make_date(aYear, aMonth, aDay), new.Product_type, new.Territory, new.Sales_Channel, new.Customer_type,
+            new.Revenue, new.Cost);
+    RETURN new;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insertoEnIntermedia
+    AFTER INSERT
+    ON intermedia
+    FOR EACH ROW
+EXECUTE PROCEDURE insertarEnDefinitiva();
+
+
+COPY intermedia FROM 'C:\Mati\DataGripProjects\TPE_BD\SalesbyRegion.csv' WITH DELIMITER ',' CSV HEADER;
+
+--4 Cálculo de la mediana
+
+CREATE OR REPLACE FUNCTION MedianaMargenMovil(fecha date, n int) RETURNS float
+AS $$
+DECLARE
+    initDate date;
+    margenDeVentas float;
+BEGIN
+    initDate := fecha - INTERVAL '1 month' * n;
+
+    SELECT percentile_disc(0.5) within group (order by Revenue - Cost)
+    INTO margenDeVentas
+    FROM definitiva
+    WHERE Sales_Date > initDate AND Sales_Date < fecha;
+
+    return margenDeVentas;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT MedianaMargenMovil(to_date('2012-11-01','YYYY-MM-DD'),4);
+
+--5 Reporte de Ventas
+CREATE OR REPLACE FUNCTION ReporteVentas(n int) RETURNS float
+AS $$
+DECLARE
+    initDate date;
+    initYear int;
+    lastYear int;
+BEGIN
+    SELECT MIN(Sales_Date) INTO initDate FROM definitiva;
+    initYear := EXTRACT(YEAR FROM initDate);
+    lastYear := initYear + n - 1;
+
+END
+$$ LANGUAGE plpgsql;
