bba98603756ec5a0a927cc00e8af57e7c928e556
Santos Rosati <srosati@itba.edu.ar>
2021-06-07 15:50:57 -0300

    Added sales channel and product type cursors and sales view

--- funciones.sql
+++ funciones.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE intermedia --TODO agregar validaciones del formato en las fechas
 (
-    Quarter       text not null,
-    Month         text not null,
-    Week          text not null,
+    Quarter       text not null CHECK ( Quarter ~ '^Q[1-4]/[0-9]{4}$' ),
+    Month         text not null CHECK ( Month ~ '^[0-9]{2}-[A-Z][a-z]{2}$' ),
+    Week          text not null CHECK ( Week ~ '^W[1-5]-[0-9]{4}$' ),
     Product_type  text not null,
     Territory     text not null,
     Sales_Channel text not null,
@@ -93,46 +93,110 @@ $$ LANGUAGE plpgsql;
 SELECT MedianaMargenMovil(to_date('2012-11-01', 'YYYY-MM-DD'), 4); --TODO los decimales e imprimir un mensaje si el parametro es 0
 
 --5 Reporte de Ventas
-CREATE OR REPLACE FUNCTION ReporteVentas(n int) RETURNS void
+CREATE VIEW salesView(Sales_Year, Product_type, Sales_Channel, Customer_type, Revenue, Cost) AS
+SELECT EXTRACT(YEAR from Sales_Date) AS Sales_Year,
+       Product_type,
+       Sales_Channel,
+       Customer_type,
+       Revenue,
+       Cost
+FROM definitiva;
+
+
+CREATE OR REPLACE FUNCTION ReporteVentas(n int) RETURNS void -- TODO: Chequear input 0 o NULL etc
 AS
 $$
 DECLARE
-    aRec     record;
-    initDate date;
-    initYear int;
-    lastYear int;
-    myCursor CURSOR FOR SELECT Sales_Date, Customer_type, Sum(Cost) AS Cost, Sum(Revenue) as Revenue
-                        FROM (SELECT EXTRACT(YEAR from Sales_Date) AS Sales_Date,
-                                     Product_type,
-                                     Sales_Channel,
-                                     Customer_type,
-                                     Revenue,
-                                     Cost
-                              FROM definitiva
-                              WHERE EXTRACT(YEAR from Sales_Date) BETWEEN initYear AND lastYear) AS auxi
-    GROUP BY Sales_date, Customer_type
-    ORDER BY Sales_Date, Customer_type;
+    aRec         record;
+    initDate     date;
+    initYear     int;
+    lastYear     int;
+    yearIdx      int;
+    totalRevenue int;
+    totalCost    int;
+    customerTypeCursor CURSOR (selectedYear int) FOR SELECT Customer_type,
+                                                            Sum(Cost)::Int    AS Cost,
+                                                            Sum(Revenue)::Int as Revenue
+                                                     FROM salesView
+                                                     WHERE Sales_Year = selectedYear
+                                                     GROUP BY Customer_type
+                                                     ORDER BY Customer_type;
+    productTypeCursor CURSOR  (selectedYear int) FOR SELECT Product_type,
+                                                            Sum(Cost)::Int    AS Cost,
+                                                            Sum(Revenue)::Int as Revenue
+                                                     FROM salesView
+                                                     WHERE Sales_Year = selectedYear
+                                                     GROUP BY Product_type
+                                                     ORDER BY Product_type;
+    salesChannelCursor CURSOR (selectedYear int) FOR SELECT Sales_Channel,
+                                                            Sum(Cost)::Int    AS Cost,
+                                                            Sum(Revenue)::Int as Revenue
+                                                     FROM salesView
+                                                     WHERE Sales_Year = selectedYear
+                                                     GROUP BY Sales_Channel
+                                                     ORDER BY Sales_Channel;
 BEGIN
     SELECT MIN(Sales_Date) INTO initDate FROM definitiva;
     initYear := EXTRACT(YEAR FROM initDate);
     lastYear := initYear + n - 1;
 
-    OPEN myCursor;
-    LOOP
-        FETCH myCursor INTO aRec;
-        EXIT WHEN NOT FOUND;
-        raise NOTICE '% % % %', aRec.Customer_type, aRec.Revenue, aRec.Cost, aRec.Revenue - aRec.Cost;
-    END LOOP;
+    yearIdx := initYear;
+
+    RAISE NOTICE '--------------------- HISTORIC SALES REPORT -----------------------';
+    RAISE NOTICE '-------------------------------------------------------------------';
+    RAISE NOTICE 'Year--Category------------------------------Revenue---Cost---Margin';
+
+    WHILE (yearIdx <= lastYear)
+        LOOP
+            totalRevenue := 0;
+            totalCost := 0;
+            RAISE NOTICE '-------------------------------------------------------------------';
+            RAISE NOTICE '%', yearIdx;
+            OPEN customerTypeCursor(selectedYear := yearIdx);
+            LOOP
+                FETCH customerTypeCursor INTO aRec;
+                EXIT WHEN NOT FOUND;
+                totalRevenue := totalRevenue + aRec.Revenue;
+                totalCost := totalCost + aRec.Cost;
+                RAISE NOTICE '--- Customer Type: % % % %', aRec.Customer_type, aRec.Revenue, aRec.Cost, aRec.Revenue - aRec.Cost;
+            END LOOP;
+            CLOSE customerTypeCursor;
+
+            OPEN productTypeCursor(selectedYear := yearIdx);
+            LOOP
+                FETCH productTypeCursor INTO aRec;
+                EXIT WHEN NOT FOUND;
+                totalRevenue := totalRevenue + aRec.Revenue;
+                totalCost := totalCost + aRec.Cost;
+                RAISE NOTICE '--- Product Type: % % % %', aRec.Product_type, aRec.Revenue, aRec.Cost, aRec.Revenue - aRec.Cost;
+            END LOOP;
+            CLOSE productTypeCursor;
+
+            OPEN salesChannelCursor(selectedYear := yearIdx);
+            LOOP
+                FETCH salesChannelCursor INTO aRec;
+                EXIT WHEN NOT FOUND;
+                totalRevenue := totalRevenue + aRec.Revenue;
+                totalCost := totalCost + aRec.Cost;
+                RAISE NOTICE '--- Sales Channel: % % % %', aRec.Sales_Channel, aRec.Revenue, aRec.Cost, aRec.Revenue - aRec.Cost;
+            END LOOP;
+            CLOSE salesChannelCursor;
+
+            RAISE NOTICE '---------------------------------------------- % % %', totalRevenue, totalCost, totalRevenue - totalCost;
+            yearIdx := yearIdx + 1;
+        END LOOP;
+
 
 END
 $$ LANGUAGE plpgsql;
 
 -----------------------
 
-DO $$
-BEGIN
- PERFORM ReporteVentas(1);
-END;
+DO
+$$
+    BEGIN
+        PERFORM ReporteVentas(1);
+    END;
 $$;
 
 DROP FUNCTION ReporteVentas;
